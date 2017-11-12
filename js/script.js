a82f95bbca8ee28c312e2a4760f0d27d37062163
Dzov <amelie.haladjian@gmail.com>
2017-11-12 19:01:11 +0100

    feat(responsive): modify about news and contact sections

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// GOOGLE MAP
 function initMap() {
   var position = {lat: 48.8797, lng: 2.309};
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -11,14 +12,15 @@ function initMap() {
 }
 
 
+// MULTISLIDER
 $('#slider').multislider({
     interval: false,
     slideAll:true
 });
 
-window.onscroll = function() {myFunction()};
 
-function myFunction() {
+// HEADER ANIAMTION
+window.onscroll = function headerAnim() {
   if (document.documentElement.scrollTop > 10) {
     document.getElementById('header').style.backgroundColor = "#30415D";
     document.getElementById('header').style.opacity = .89; 
@@ -27,10 +29,11 @@ function myFunction() {
   }
 }
 
+
+// SMOOTHSCROLL
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
     
-    //smoothscroll
     $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
         $(document).off("scroll");
@@ -52,13 +55,12 @@ $(document).ready(function () {
     });
 });
 
+// ACTIVE NAV LINK
 function onScroll(event){
     var scrollPos = $(document).scrollTop();
-    console.log('scrollPos' + scrollPos); 
     $('#nav a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        console.log(refElement.position().top); 
         if (refElement.position().top - 150 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
             $('#nav ul li a').removeClass("active");
             currLink.addClass("active");
