b90f457a86204b98d5f5c34634563bf784228e80
Dzov <amelie.haladjian@gmail.com>
2017-11-16 17:32:00 +0100

    feat(*): add responsive

--- js/script.js
+++ js/script.js
@@ -14,59 +14,71 @@ function initMap() {
 
 // MULTISLIDER
 $('#slider').multislider({
-    interval: false,
-    slideAll:true
+  interval: false,
+  slideAll:true
 });
 
 
-// HEADER ANIAMTION
+// HEADER ANIMATION ON SCROLL
 window.onscroll = function headerAnim() {
   if (document.documentElement.scrollTop > 10) {
     document.getElementById('header').style.backgroundColor = "#30415D";
     document.getElementById('header').style.opacity = .89; 
   } else {
-    document.getElementById('header').style.backgroundColor = "transparent";
+    if (document.documentElement.clientWidth > 875) {
+      document.getElementById('header').style.backgroundColor = "transparent";
+    }
   }
 }
 
-
 // SMOOTHSCROLL
 $(document).ready(function () {
-    $(document).on("scroll", onScroll);
-    
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-      
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
+  $(document).on("scroll", onScroll);
+
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+
+    $('a').each(function () {
+      $(this).removeClass('active');
+    })
+    $(this).addClass('active');
+
+    var target = this.hash,
+    menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top+2
+    }, 500, 'swing', function () {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
     });
+  });
 });
 
 // ACTIVE NAV LINK
 function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('#nav a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top - 150 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#nav ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
+  var scrollPos = $(document).scrollTop();
+  $('#nav a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top - 150 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+      $('#nav ul li a').removeClass("active");
+      currLink.addClass("active");
+    }
+    else{
+      currLink.removeClass("active");
+    }
+  });
 }
+
+
+// RESPONSIVE FILMS SECTION
+$(window).resize(function() {
+  if (document.documentElement.clientWidth < 750) {
+    document.querySelector('#MS-content').classList.remove('MS-content'); 
+  } else if (!document.querySelector('#MS-content').classList.contains('MS-content')) {
+    document.querySelector('#MS-content').classList.add('MS-content');
+  }
+});
+
